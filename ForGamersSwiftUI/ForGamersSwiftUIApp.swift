6c00c74820aaeea560e7fa47bf7121d41fdf280b
Aaron Treinish <aarontreinish@fastmail.com>
2021-06-02 06:43:43 -0400

    Initial Commit

---
+++ ForGamersSwiftUI/ForGamersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForGamersSwiftUIApp.swift
+//  ForGamersSwiftUI
+//
+//  Created by Aaron Treinish on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ForGamersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
